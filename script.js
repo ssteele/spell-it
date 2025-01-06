00bcfad9a644f3783ab453041d68894c759aaf58
Steve Steele <steven.harville.steele@gmail.com>
2025-01-05 22:42:53 -0600

    Remove unnecessary class names

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function SpellItApp() {
   return (
     <>
       <input
-        className="input fixed p-2 top-4 right-4"
+        className="fixed p-2 top-4 right-4"
         id="input"
         placeholder="Spell it"
         type="text"
@@ -49,11 +49,11 @@ function SpellItApp() {
         onChange={(e) => setInput(e?.target?.value)}
       />
       
-      <section className="body flex h-screen mx-12">
+      <section className="flex h-screen mx-12">
         {hintCount > 0 && (
           <section className="grid grid-cols-1 gap-y-20 content-center">
             {hints.map((letter, idx) => (
-              <section className="abc" key={idx}>
+              <section key={idx}>
                 {1 === letter.length && (
                   <span className="text-2xl font-bold">{letter}</span>
                 )}
@@ -66,15 +66,15 @@ function SpellItApp() {
           </section>
         )}
 
-        <section className="main grid grid-cols-1 gap-y-8 w-full items-center justify-items-center">
-          <section className="target">
+        <section className="grid grid-cols-1 gap-y-8 w-full items-center justify-items-center">
+          <section>
             <img
               className="h-72 w-72"
               src={`img/targets/${target}.svg`}
             />
           </section>
 
-          <section className="word h-32 text-9xl">{input}</section>
+          <section className="h-32 text-9xl">{input}</section>
         </section>
       </section>
     </>
