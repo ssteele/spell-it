9f9bd953cc533c97e5fea4ede59e0adaca3a614e
Steve Steele <steven.harville.steele@gmail.com>
2025-01-26 09:06:19 -0600

    Remove hardcoded words: now deprecated

--- src/Constants/Words.jsx
+++ src/Constants/Words.jsx
@@ -1,13 +1,1 @@
 export const SupportedLanguages = ['en', 'es'];
-
-export const WordList = {
-  en: [
-    'ant', 'bag', 'bat', 'bed', 'box', 'bug', 'bus', 'can', 'car', 'cat', 'cow', 'cup', 'dog', 'fan', 'fox', 'hat', 'hen', 'jar', 'leg',
-    'map', 'net', 'owl', 'pan', 'pig', 'pot', 'run', 'saw', 'six', 'sun', 'ten', 'top', 'tub', 'wet',
-  ],
-  es: [
-    'agua', 'baño', 'boca', 'bote', 'caja', 'cama', 'casa', 'dos', 'foca', 'fotos', 'gato', 'jugo', 'lobo', 'luna', 'mano', 'mesa', 'mono',
-    'moto', 'nido', 'nube', 'ojos', 'pala', 'pan', 'pata', 'pato', 'pelo', 'pie', 'piña', 'queso', 'rana', 'sol', 'sopa', 'taza', 'toro',
-    'tren', 'tres', 'uno', 'vaca',
-  ],
-};
