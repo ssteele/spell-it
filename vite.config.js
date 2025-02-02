be959a919c1e336d31c9269f09eb4e94b3a4419f
Steve Steele <steven.harville.steele@gmail.com>
2025-02-02 14:49:36 -0600

    Attempt to fix npm run build
    Top-level await is not available in the configured target environment
    
    const db = await getDatabase();

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    supported: {
+      'top-level-await': true // browsers can handle top-level-await features
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
